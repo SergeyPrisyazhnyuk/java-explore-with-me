daa4a3576c2459c8971abea3d0e1cdc3cac6aae1
SergeyPrisyazhnyuk <130796016+SergeyPrisyazhnyuk@users.noreply.github.com>
2024-01-25 22:08:27 +0300

    feat : main_srv Iter 13

--- main-service/src/main/java/ru/practicum/ewm/dto/LocationDto.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/LocationDto.java
@@ -1,9 +1,6 @@
 package ru.practicum.ewm.dto;
 
 import lombok.*;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Getter
